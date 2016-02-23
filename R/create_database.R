565398a82c6cbfa5c64b37ee84edc57472125001
Your Name <bczernecki@gmail.com>
2016-02-23 21:55:08 +0100

    poprawa joinowania

--- R/create_database.R
+++ R/create_database.R
@@ -1,3 +1,5 @@
+library(plyr)
+
 # load all archive dataset from local directory
 
 # define constants:
@@ -6,41 +8,58 @@
 header <- c("PRES","HGHT","TEMP","DWPT","RELH","MIXR","DRCT","SKNT","THTA","THTE","THTV")
 
 # TODO:
-# structure for stats:
-stations <- dir(path="data/sondowania")
+setwd("~/Pulpit/")
+stations <- dir(path="sondowania")
 
 # beginning of loop for folders/stations:
 nowa_lista <- list()
 licznik <- 1
 for (i in stations){
         
-        files <- dir(paste0("data/sondowania/", i), full.names = TRUE)
+        files <- dir(paste0("sondowania/", i), full.names = TRUE)
         
         for (j in files){
         #j <- files[1]
         
         tmp <- readLines(j) # temporary object for raw data
+        
+        if(length(tmp)>28){ # jak pomiar jest krotszy niz 28 linii (w rzeczywistoscii 5 poziomow to nie bierz sondowania pod uwage)
+        
         term_lin <- grep(pattern="Station information",tmp)
-        term_lin_stat <- grep(pattern="Precipitable",tmp)-term_lin
+        term_lin_stat <- grep(pattern="Description",tmp)-term_lin-1
         res <- read.fwf(j, widths = c(7,7,7,7,7,7,7,7,7,7,7), skip=5, n = term_lin-6,header = F)
         colnames(res) <- header
         
         # staty bez daty i kodu stacji
         staty <- as.double(as.character(read.fwf(j, widths = c(44,20), skip=term_lin+2, header = F, n = term_lin_stat-2)[,2]))
-        date <- strptime(paste(substr(j, 29,38),substr(j, 40,41)), "%Y.%m.%d %H", tz="UTC")
+        test <- read.fwf(j, widths = c(44,20), skip=term_lin+2, header = F, n = term_lin_stat-2)
+        
+        if(licznik==1) {
+          test2 <- test
+        }
+        staty <- join(test2,test, by="V1")[,3]
+        names(staty) <- sub(":", "", gsub(" *", '',as.character(test2[,1])))
+        
+        
+        #if(length(staty)<23) stop("bubu")
+        
+        date <- strptime(paste(substr(j, 24,33),substr(j, 35,36)), "%Y.%m.%d %H", tz="UTC")
         station <- i
         
         tmp_list <- list(station=station,date=date, res=res,staty=staty)
+      
         
         # ok., tutaj jest problem ze zlaczeniem 2 list o podobnej postaci, zeby pozniej sie dalo
         # w miare latwo na czyms takim lapply'owac:
         
         nowa_lista[[licznik]] <- tmp_list
+         
         print(j) 
         licznik <- licznik+1
+        }# koniec ifa dla pustych plikow
         }
 }
 
 
-saveRDS(nowa_lista, file="~/Pulpit/sondowania/baza.rds")
+saveRDS(nowa_lista, file="~/Dokumenty/publikacje_własne/2016/sondowania/baza.rds")
 # podejrzyj strukture nowej listy....
--- R/staty_giver_lista.R
+++ R/staty_giver_lista.R
@@ -6,4 +6,5 @@ staty_giver_lista <- function(listewka){
 }
 
 moje_staty <- staty_giver_lista(nowa_lista)
-lapply(moje_staty, function(x) x[5]) #Piąty element (1997) - Korben Dallas to były komandos, a obecnie taksówkarz w NY w XXIII wieku. Wplątany zostaje w misję ratowania świata przed siłami zła ...
+table(names(unlist(lapply(moje_staty, function(x) x[5])))) #Piąty element (1997) - Korben Dallas to były komandos, a obecnie taksówkarz w NY w XXIII wieku. Wplątany zostaje w misję ratowania świata przed siłami zła ...
+
