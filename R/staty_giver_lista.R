b88c865640df8262b7ff9958a6d20697b1d105f7
Your Name <bczernecki@gmail.com>
2016-02-24 01:33:14 +0100

    interpolacja

--- R/staty_giver_lista.R
+++ R/staty_giver_lista.R
@@ -1,3 +1,5 @@
+library(parallel)
+
 baza <- readRDS(file="~/Dokumenty/publikacje_własne/2016/sondowania/baza.rds")
 
 staty_giver_lista <- function(listewka){
@@ -29,7 +31,8 @@ sondaz_giver_lista <- function(listewka){
 }
 
 
-sondaze <- sondaz_giver_lista(baza)
+
+
 
 moje_staty <- staty_giver_lista(baza)
 table(names(unlist(lapply(moje_staty, function(x) x[6])))) 
@@ -39,3 +42,32 @@ table(round(unlist(lapply(moje_staty, function(x) x[12]))/10)) # w cape'y rzedy 
 
 (date_giver_lista(baza))[[156192]]
 baza[[156192]]
+
+
+
+
+sondaze <- sondaz_giver_lista(baza)
+dane <- sondaze[[1]]
+dane2 <- sondaze
+
+poziomy=rev(sort(c(seq(from=700,to=1000,by=25),seq(from=100,to=650,by=50))))
+
+interpolacja_hgt <- function(dane, nr_kolumny){
+  # poziomy lepiej zadeklarowac poza funkcja
+  if(length(which(dane[,1]<1000 & dane[,1]>850))>=2) {
+    round(approx(dane[,1],dane[,nr_kolumny],xout=poziomy)$y,2)  
+} else {
+  rep(NA, length(poziomy))
+}
+}
+
+
+
+
+Rprof()
+#cl <- makeCluster(11,type = 'FORK')
+a <- lapply(dane2, interpolacja_hgt, 3)
+#a <- sapply(dane2, interpolacja_hgt)
+#stopCluster(cl)
+Rprof(NULL)
+summaryRprof()
